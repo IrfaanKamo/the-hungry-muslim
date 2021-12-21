f90681c3930fcf4e565b6c4307c44c7b239ca5b5
Irfaan Kamroodeen <irfaank@privateproperty.co.za>
2021-12-21 12:39:41 +0200

    Added initial button layout

--- web-thehungrymuslim/src/components/Product.tsx
+++ web-thehungrymuslim/src/components/Product.tsx
@@ -9,41 +9,37 @@ export interface ProductProps {
 const Product: React.FC<ProductProps> = ({product}) => {
     const [showIngredients, setShowIngredients] = useState<boolean>(false);
     
-    const toggleIngredients = () => {
-        setShowIngredients(!showIngredients);
+    const toggleIngredients = (show: boolean) => {
+        setShowIngredients(show);
     }
 
     return (
-        <div className="flex-1 border-2 rounded-lg mb-2 mx-1 p-3" >
+        <div className="flex-1 border-2 rounded-lg divide-y mb-2 mx-1 p-3" >
             
             <div className="text-xl font-semibold pb-5">{product.title}</div>
 
-            <div className={`w-24 h-6 flex items-center rounded-full p-1 ${showIngredients ? 'justify-start bg-green-200' : 'justify-end bg-yellow-200'}`} onClick={toggleIngredients}>
-                {
-                    showIngredients ? <></> : (<div className="text-xs select-none font-semibold">Markings</div>)
-                }
-                <div className="bg-white w-4 h-4 rounded-full shadow-md mx-1"></div>
-                {
-                    showIngredients ? (<div className="text-xs select-none font-semibold">Ingredients</div>) : <></>
-                }
-            </div>
-
-            <div className={showIngredients ? "hidden" : undefined}>
-                <ProductMarking 
-                    labelStatus={product.isHalaal ? AlertIcon.Safe : AlertIcon.Danger} 
-                    labelText={product.isHalaal ? 'Is Halaal' : 'Not Halaal'} 
-                />
-                <ProductMarking 
-                    labelStatus={product.isVegan ? AlertIcon.Safe : AlertIcon.Warning} 
-                    labelText={product.isVegan ? 'Is Vegan' : 'Not Vegan'} 
-                />
-                <ProductMarking 
-                    labelStatus={product.isVegetarian ? AlertIcon.Safe : AlertIcon.Warning} 
-                    labelText={product.isVegetarian ? 'Is Vegetarian' : 'Not Vegetarian'} 
-                />
-            </div>
-            <div className={showIngredients ? undefined : "hidden"}>
-                {product.ingredients}
+            <div className="flex justify-between">
+                <div className={showIngredients ? "hidden" : undefined}>
+                    <ProductMarking 
+                        labelStatus={product.isHalaal ? AlertIcon.Safe : AlertIcon.Danger} 
+                        labelText={product.isHalaal ? 'Is Halaal' : 'Not Halaal'} 
+                    />
+                    <ProductMarking 
+                        labelStatus={product.isVegan ? AlertIcon.Safe : AlertIcon.Warning} 
+                        labelText={product.isVegan ? 'Is Vegan' : 'Not Vegan'} 
+                    />
+                    <ProductMarking 
+                        labelStatus={product.isVegetarian ? AlertIcon.Safe : AlertIcon.Warning} 
+                        labelText={product.isVegetarian ? 'Is Vegetarian' : 'Not Vegetarian'} 
+                    />
+                </div>
+                <div className={showIngredients ? undefined : "hidden"}>
+                    {product.ingredients}
+                </div>
+                <div className="flex flex-col justify-center">
+                    <div className={`border-l-2 border-b-2 pl-1 select-none ${showIngredients ? 'bg-gray-200' : 'cursor-pointer'}`} onClick={() => toggleIngredients(true)}>I</div>
+                    <div className={`border-l-2 border-b-2 pl-1 select-none ${showIngredients ? 'cursor-pointer' : 'bg-gray-200'}`} onClick={() => toggleIngredients(false)}>M</div>
+                </div>
             </div>
 
         </div>
