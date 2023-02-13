3c99902e39ffe15a7bb8314b580f853593ea746b
IrfaanK <irfaan.kamo@gmail.com>
2023-02-13 14:45:46 +0200

    Updated styling of the search bar

--- web-thehungrymuslim/src/App.tsx
+++ web-thehungrymuslim/src/App.tsx
@@ -1,14 +1,14 @@
-import ProductsViewer from './components/ProductsViewer';
+import ProductsViewer from "./components/ProductsViewer";
 
 const App = () => {
-  return (
-    <div className="container mx-auto bg-gr px-5 sm:px-20 xl:px-48">
-      <div className="pt-10 text-center font-semibold text-xl">
-        The Hungry Muslim
-      </div>
-      <ProductsViewer />
-    </div>
-  );
-}
+    return (
+        <div className="container mx-auto bg-gr px-5 sm:px-20 xl:px-48">
+            <div className="pt-10 text-center font-medium text-5xl">
+                The Hungry Muslim
+            </div>
+            <ProductsViewer />
+        </div>
+    );
+};
 
 export default App;
--- web-thehungrymuslim/src/components/SearchBar.tsx
+++ web-thehungrymuslim/src/components/SearchBar.tsx
@@ -1,23 +1,40 @@
-import { RiSearchLine } from 'react-icons/ri';
+import { RiSearchLine } from "react-icons/ri";
+import classNames from "classnames";
 
 interface SearchBarProps {
-    onChange: (event: any) => void
+    onChange: (event: any) => void;
 }
 
-const SearchBar: React.FC<SearchBarProps> = ({onChange}) => {
+const SearchBar: React.FC<SearchBarProps> = ({ onChange }) => {
     return (
-        <div className="w-full h-10 pl-3 pr-2 bg-white border-2 border-indigo-300 hover:border-indigo-700 rounded-full flex justify-between items-center relative">
-            <input id="search" 
-                   placeholder="Search for products..." 
-                   className="appearance-none w-full outline-none focus:outline-none active:outline-none"
-                   onChange={onChange}
+        <div
+            className={classNames(
+                "w-full h-10 pl-3",
+                "bg-white border-2 border-gray-300 rounded-full",
+                "flex justify-between items-center relative",
+                "overflow-hidden"
+            )}
+        >
+            <input
+                id="search"
+                placeholder="Enter product name..."
+                className="appearance-none w-full outline-none focus:outline-none active:outline-none"
+                onChange={onChange}
             />
-                   
-            <button type="submit" className="mr-3">
-                <RiSearchLine size={20}/>
+
+            <button
+                type="submit"
+                className={classNames(
+                    "px-6",
+                    "text-white bg-green-300 h-10 border-l-1 border-black hover:bg-green-400",
+                    "flex justify-center items-center"
+                )}
+            >
+                <RiSearchLine size={17} color="white" />
+                <span className="pl-2 font-semibold">Find</span>
             </button>
         </div>
-    )
+    );
 };
 
 export default SearchBar;
